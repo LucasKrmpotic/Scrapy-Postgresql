bb276efb175cf33e824051a71c1609b51a3d6b94
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-10-14 23:43:48 -0300

    spiders empezadas

--- codeworkers/items.py
+++ codeworkers/items.py
@@ -5,10 +5,10 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
-
-class CodeworkersItem(scrapy.Item):
+class CodeworkersItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    titulo = Field()
+    url = Field()
--- codeworkers/settings.py
+++ codeworkers/settings.py
@@ -24,6 +24,9 @@ ROBOTSTXT_OBEY = True
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
+
+#CSV IMPORTACION
+ITEM_PIPELINES = {'codeworkers.pipelines.CodeworkersPipeline': 500,}
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
