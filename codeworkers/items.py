dc0f03987a8f98634c9a2eee1a8da98b7e05148d
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-10-14 23:53:10 -0300

    Revert "spiders empezadas"
    This reverts commit bb276efb175cf33e824051a71c1609b51a3d6b94.

--- codeworkers/items.py
+++ codeworkers/items.py
@@ -5,10 +5,10 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-from scrapy.item import Item, Field
+import scrapy
 
-class CodeworkersItem(Item):
+
+class CodeworkersItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    titulo = Field()
-    url = Field()
+    pass
