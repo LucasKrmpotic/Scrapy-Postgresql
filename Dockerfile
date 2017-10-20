22f1394bb2fe6dabf393c9b61f8effa913b21fa2
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-10-20 01:57:41 +0000

    removiendo basura

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python
-RUN apt-get update -q
-COPY . .
-RUN pip install -r requirements.txt
-CMD ["bash", "cron-job.sh"] 
-
--- crawler/cron-job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrapy crawl computrabajo -s CLOSESPIDER_ITEMCOUNT=50
-scrapy crawl bumeran -s CLOSESPIDER_ITEMCOUNT=50
-scrapy crawl zonajobs -s CLOSESPIDER_ITEMCOUNT=50
-scrapy crawl lawebdelprogramador -s CLOSESPIDER_ITEMCOUNT=50
-scrapy crawl stackoverflow -s CLOSESPIDER_ITEMCOUNT=50 
